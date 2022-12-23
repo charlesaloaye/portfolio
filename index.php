6523bef2e284ed46f20b9dbd9affde5c2393e1ef
Sedenu <aloayecharles@gmail.com>
2022-12-23 21:30:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
    include"inc/header.php";
-   ?>
-<?php include"inc/nav.php";?>    
-<?php
-include"inc/body.php";?>
-<?php include"inc/footer.php";?>
+   include"inc/nav.php";  
+   include"inc/body.php";
+   include"inc/footer.php";
